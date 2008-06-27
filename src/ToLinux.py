cfbe917a2de2adc83b5b66428c4455ba7a4b0ec7
SiloDS <SiloDS@gmail.com>
2008-06-27 10:53:57 +0000

    Note: Added ToLinux.py
    git-svn-id: file:///srv/svn/RToolDS@1348 f4f37104-117a-2c40-a1c3-40b932acf59f

---
+++ src/ToLinux.py
@@ -0,0 +1,25 @@
+from cx_Freeze import setup, Executable
+
+setup(
+        name = "RToolDS",
+        version = "0.3",
+        description = "RToolDS",
+        executables = [Executable("RToolDS.py")]
+)
+
+#
+#setup( 
+#        name = "RToolDS", 
+#        options = {"py2exe": {"compressed": 1, 
+#                              "optimize": 2, 
+#                              "bundle_files": 1}}, 
+#        windows = [
+#        {
+#            "script": "RToolDS.py", 
+#            "icon_resources": [( 1, os.path.join ("..","gfx","GFX_Icon.ico") )], 
+#            "other_resources": [( 24, 1, manifest )]
+#        }
+#    ], 
+#        data_files=[( ".", 
+#                   ["ReadMe.txt", "License.txt", "unrar.dll", "RToolDS_Trimmed.dat.new", "ADVANsCEne_RToolDS.xml"] )]
+# )
